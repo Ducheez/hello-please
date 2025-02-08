6b9278cf6cfbb56f2394a93590520fab9f293f3a
Ducheez <njokivanessa2005@gmail.com>
2025-02-08 22:22:17 +0300

    Third commit

--- src/components/IntroScreen.jsx
+++ src/components/IntroScreen.jsx
@@ -2,7 +2,7 @@ import React from "react"
 function IntroScreen({ onNext }) {
     return (
       <div className="intro-screen">
-        <h1 className="pixel-text text-4xl text-pink-400 mb-8">Hey Gumball, ready for a surprise?</h1>
+        <h1 className="pixel-text text-4xl text-pink-400 mb-8">Hey Shanice, ready for a surprise?</h1>
         <img
           src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/heart-spin-0KvW5Y8LEZGBlOt6pS09nRglwZWixG.gif"
           alt="Bouncing Heart"
